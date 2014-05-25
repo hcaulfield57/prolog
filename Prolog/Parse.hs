2f76137c48107b2086cc2c91868cf33d1407629c
Grant Mather <hcaulfield57@gmail.com>
2014-05-25 16:13:29 -0600

    attempting to fix formatting

--- Prolog/Parse.hs
+++ Prolog/Parse.hs
@@ -2,6 +2,7 @@ module Prolog.Parse (parseDate, parseHTML) where
 
 import Data.List
 import Text.HTML.TagSoup
+import Text.PrettyPrint
 
 parseDate :: String -> Maybe (Int, Int)
 parseDate date
@@ -14,4 +15,32 @@ parseDate date
     | otherwise = Nothing
 
 parseHTML :: String -> String
-parseHTML html = innerText $ parseTags html
+parseHTML html =
+    let sty = Style PageMode 80 1.5
+        par = renderStyle sty $ prettyHTML (parseTags html)
+    in tabs . newline $ par
+  -- some of the formatting that these files come in is
+  -- pretty awful, this is an attempt to fix it however
+  -- it doesn't help a ton, needs some work.
+  where tabs (x:xs) = 
+            if x == '\t'
+            then ' ' : tabs xs
+            else x : tabs xs
+        tabs [] = []
+
+        newline (x1:x2:xs) =
+            if x1 == x2
+            then x1 : newline xs
+            else x1 : x2 : newline xs
+        newline (x:[]) = [x]
+        newline [] = []
+
+prettyHTML :: [Tag String] -> Doc
+prettyHTML [] = empty
+prettyHTML (t:ts) = case t of
+    (TagOpen tag _) -> case tag of
+        "P" -> char '\n' <> prettyHTML ts
+        "p" -> char '\n' <> prettyHTML ts
+        _   -> prettyHTML ts
+    (TagText txt)   -> text txt <> prettyHTML ts
+    _               -> prettyHTML ts
